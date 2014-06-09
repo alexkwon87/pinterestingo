3cd066fafe74d3577b229c9633bddd23650474af
Alex Kwon <alexkwon87@gmail.com>
2014-06-09 06:34:39 -0500

    scaffold added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" =>"pages#about" #creates about_path
@@ -57,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  
 end
